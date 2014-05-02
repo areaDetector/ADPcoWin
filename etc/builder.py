d9caf03018ae09aa5cd12059db0ccb857a3dfaad
Jonathan Thompson <Jonathan.Thompson@diamond.ac.uk>
2014-05-02 11:43:06 +0000

    pcocam2: Added support for min/max exposure timing.

--- etc/builder.py
+++ etc/builder.py
@@ -1,28 +1,24 @@
 
-from iocbuilder import Device, AutoSubstitution, Architecture, SetSimulation
+from iocbuilder import Device, AutoSubstitution, Architecture
 from iocbuilder.arginfo import *
 
 from iocbuilder.modules.areaDetector import AreaDetector, _ADBase, _ADBaseTemplate, simDetector
 
-class _pcocam(AutoSubstitution):
-    TemplateFile="pcocam.template"
+class _pcocam2(AutoSubstitution):
+    TemplateFile="pco.template"
     SubstitutionOverwrites = [_ADBaseTemplate]
     
-class pcocam(_ADBase):
+class pcocam2(_ADBase):
     """Create a PCO camera detector"""
-    _SpecificTemplate = _pcocam
-    def __init__(self, PCOWIDTH, PCOHEIGHT, BUFFERS=50, MEMORY=-1, **args):
+    _SpecificTemplate = _pcocam2
+    def __init__(self, BUFFERS=50, MEMORY=-1, **args):
         self.__super.__init__(**args)
         self.__dict__.update(locals())
         
     # __init__ arguments
     ArgInfo = _ADBase.ArgInfo + _SpecificTemplate.ArgInfo + makeArgInfo(__init__,
-        PCOWIDTH = Simple('PCO sensor width', int),
-        PCOHEIGHT  = Simple('PCO sensor height', int),
-        BUFFERS = Simple('Maximum number of NDArray buffers to be created for '
-            'plugin callbacks', int),
-        MEMORY  = Simple('Max memory to allocate, should be maxw*maxh*nbuffer '
-            'for driver and all attached plugins', int))
+        BUFFERS = Simple('Maximum number of NDArray buffers to be created', int),
+        MEMORY  = Simple('Max memory to allocate', int))
     LibFileList = []
     DbdFileList = []
     SysLibFileList = []
@@ -31,16 +27,12 @@ class pcocam(_ADBase):
     # For any windows architecture, install the pcocam libraries
     # and configure the required linker flags
     if epics_host_arch.find('win') >= 0:
-        LibFileList += ['pcocamSupport', 'SC2_DLG', 'SC2_Cam','PCO_CDLG','Pco_conv' ]
-        SysLibFileList += ['nafxcw']
-        DbdFileList += ['pcocamSupport']
+        LibFileList += ['pcocam2', 'SC2_DLG', 'SC2_Cam','PCO_CDLG','Pco_conv' ]
+        SysLibFileList += ['windowscodecs', 'Comdlg32', 'Winspool', 'Comctl32', 'nafxcw']
+        DbdFileList += ['pcocam2Support', 'pcocam2HardwareSupport']
         MakefileStringList += ['%(ioc_name)s_LDFLAGS_WIN32 += /NOD:nafxcwd.lib /NOD:nafxcw.lib']
     
     def Initialise(self):
-        print '# pcocam_config(   portName, maxSizeX, maxSizeY, maxBuffers, maxMemory )'
-        print '  pcocam_config( %(PORT)10s, %(PCOWIDTH)8s, %(PCOHEIGHT)8s, %(BUFFERS)10d, %(MEMORY)9d )' % self.__dict__
+        print '  pcoConfig( %(PORT)10s, %(BUFFERS)10d, %(MEMORY)9d )' % self.__dict__
+        print '  pcoApiConfig( %(PORT)10s )' % self.__dict__
 
-def pcocam_sim(PCOWIDTH, PCOHEIGHT, **kwargs):
-    return simDetector( WIDTH=PCOWIDTH, HEIGHT=PCOHEIGHT, **kwargs)
-
-SetSimulation(pcocam, pcocam_sim)
