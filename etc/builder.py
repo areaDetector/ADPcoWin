246688649911c01edeb04e8b80064eb7d342e23f
tdq39642 <tdq39642@diamond.ac.uk>
2017-03-28 10:17:05 +0000

    Builder definitions for Gang server and client

--- etc/builder.py
+++ etc/builder.py
@@ -43,9 +43,55 @@ class pcocam2(AsynPort):
             MakefileStringList += ['%(ioc_name)s_LDFLAGS_WIN32 += /NOD:nafxcwd.lib /NOD:nafxcw.lib']
 
     def Initialise(self):
+        print '# pcoConfig(portName, buffers, memory)'
         print 'pcoConfig("%(PORT)s", %(BUFFERS)d, %(MEMORY)d)' % self.__dict__
         if self.epics_host_arch.find('win') >= 0:
+            print '# pcoApiConfig(portName)'
             print 'pcoApiConfig("%(PORT)s")' % self.__dict__
         else:
+            print '# simulationApiConfig(portName)'
             print 'simulationApiConfig("%(PORT)s")' % self.__dict__
+
+class _pcocam2GangServer(AutoSubstitution):
+    TemplateFile = "pco_gangserver.template"
+
+class pcocam2GangServer(Device):
+    _SpecificTemplate = _pcocam2GangServer
+    AutoInstantiate = True
+
+    def __init__(self, LISTENINGTCPPORT, PORT, **args):
+        self.__super.__init__()
+        self.__dict__.update(locals())
+        makeTemplateInstance(self._SpecificTemplate, locals(), args)
+
+    ArgInfo = _SpecificTemplate.ArgInfo + makeArgInfo(
+        __init__,
+        PORT = Ident('The asyn port name of the detector driver', pcocam2),
+        LISTENINGTCPPORT = Simple('A TCP port number for the server to listen on', int))
+
+    def Initialise(self):
+        print '# gangServerConfig(portName, listeningTcpPort)'
+        print 'gangServerConfig("%(PORT)s", %(LISTENINGTCPPORT)d)' % self.__dict__
+
+class _pcocam2GangClient(AutoSubstitution):
+    TemplateFile = "pco_gangconnection.template"
+
+class pcocam2GangClient(Device):
+    _SpecificTemplate = _pcocam2GangClient
+    AutoInstantiate = True
+
+    def __init__(self, SERVERIP, SERVERTCPPORT, PORT, **args):
+        self.__super.__init__()
+        self.__dict__.update(locals())
+        makeTemplateInstance(self._SpecificTemplate, locals(), args)
+
+    ArgInfo = _SpecificTemplate.ArgInfo + makeArgInfo(
+        __init__,
+        PORT = Ident('The asyn port name of the detector driver', pcocam2),
+        SERVERIP = Simple('The IP address of the gang server', str),
+        SERVERTCPPORT = Simple('A TCP port number of the gang server', int))
+
+    def Initialise(self):
+        print '# gangConnectionConfig(portName, serverIp, serverTcpPort)'
+        print 'gangConnectionConfig("%(PORT)s", "%(SERVERIP)s", %(SERVERTCPPORT)d)' % self.__dict__
 
